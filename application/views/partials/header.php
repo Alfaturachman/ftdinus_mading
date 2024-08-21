d4de2ea9f03330155a2a1f4f3d478d0bfaf736f2
alfaturachman <122202202929@mhs.dinus.ac.id>
2024-08-21 15:26:16 +0700

    initial commit

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -16,6 +16,8 @@
     <link rel="stylesheet" href="<?= base_url('') ?>assets/css/animate.css" />
     <link rel="stylesheet" href="<?= base_url('') ?>assets/css/tailwind.css" />
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     <!-- ==== WOW JS ==== -->
     <script src="<?= base_url('') ?>assets/js/wow.min.js"></script>
     <script>
@@ -30,7 +32,7 @@
         <div class="container mx-auto px-4">
             <div class="relative flex items-center justify-between">
                 <div class="w-60 max-w-full px-4">
-                    <a href="index.html" class="navbar-logo block w-full py-5">
+                    <a class="navbar-logo block w-full py-5" href="<?= site_url('home') ?>">
                         <img
                             src="<?= base_url('') ?>assets/images/logo/logo-white.svg"
                             alt="logo"
@@ -38,8 +40,23 @@
                     </a>
                 </div>
                 <div class="flex w-full items-center justify-between px-4">
-                    <div class="flex items-center justify-end pr-16 lg:pr-0">
-                        <!-- Konten navbar di sini, misalnya menu navigasi -->
+                    <div class="flex items-center w-full">
+                        <!-- Menu navigasi dan tombol Cari -->
+                        <div class="flex space-x-4 w-full items-center justify-end">
+                            <!-- Menu navigasi -->
+                            <nav class="flex space-x-4 mr-3 items-center gap-4">
+                                <a href="<?= site_url('daftar') ?>"
+                                    class="text-white hover:text-gray-300 duration-300 ease-in-out">
+                                    Home
+                                </a>
+                                <a
+                                    href="<?= site_url('home') ?>"
+                                    class="signUpBtn rounded-md bg-white bg-opacity-20 px-6 py-2 text-base font-medium text-white duration-300 ease-in-out hover:bg-opacity-100 hover:text-dark flex items-center space-x-2">
+                                    <i class="fas fa-search mr-3"></i>
+                                    <span>Cari</span>
+                                </a>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -53,6 +70,5 @@
             top: 0;
             left: 0;
             width: 100%;
-            padding: 1rem 2rem;
         }
     </style>
