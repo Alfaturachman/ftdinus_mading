9488700ed0652c5f01563593ec8e2a041fa1abc6
alfaturachman <122202202929@mhs.dinus.ac.id>
2024-08-22 10:46:42 +0700

    initial commit

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-<<<<<<< HEAD
     public function __construct()
     {
         parent::__construct();
@@ -15,7 +14,3 @@ class Auth extends CI_Controller
     }
 }
 
-=======
-    
-}
->>>>>>> a831f187d94b6363c50b5abcb959cf1784e1e161
